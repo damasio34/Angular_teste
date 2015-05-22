9bd8569acd457676dfb024dc2424a26beb563933
unknown <darlan.damasio@PAT-000022.vvssistemas.local>
2015-05-22 13:04:10 -0300

    Inclusão da index e do bootstrap do sistema

---
+++ app/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//módulo root do app
+angular.module('App', []);
+
+// Bootstrap do app
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['myApp']);
+});
